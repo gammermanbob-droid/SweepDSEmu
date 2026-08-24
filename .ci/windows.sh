4f861847d77e52ef1fbfa99b158863d596b6602c
Bryden King <gammermanbob@gmail.com>
2026-08-24 01:00:18 -0400

    Revert "ci(windows): explicitly point CMake at vcpkg's toolchain file"
    This reverts commit 0e8db02e207013ca575e31bb2afaeab9fe7838c9.

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -7,19 +7,12 @@ if [ "$GITHUB_REF_TYPE" == "tag" ]; then
 	export EXTRA_CMAKE_FLAGS=(-DENABLE_QT_UPDATE_CHECKER=ON)
 fi
 
-if [ -n "$VCPKG_INSTALLATION_ROOT" ]; then
-	VCPKG_TOOLCHAIN="$VCPKG_INSTALLATION_ROOT/scripts/buildsystems/vcpkg.cmake"
-elif [ -n "$VCPKG_ROOT" ]; then
-	VCPKG_TOOLCHAIN="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
-fi
-
 cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -DENABLE_QT_TRANSLATION=ON \
     -DUSE_DISCORD_PRESENCE=ON \
-    ${VCPKG_TOOLCHAIN:+-DCMAKE_TOOLCHAIN_FILE="$VCPKG_TOOLCHAIN"} \
 	"${EXTRA_CMAKE_FLAGS[@]}"
 ninja
 ninja bundle
